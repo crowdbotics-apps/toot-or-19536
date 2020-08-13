e9116c202fedefae591301d489432f33c302bc1a
Crowdbotics <team@crowdbotics.com>
2020-08-13 12:26:26 +0000

    Initial commit.

---
+++ ios/toot_or_19536Tests/toot_or_19536Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface toot_or_19536 : XCTestCase
+
+@end
+
+@implementation toot_or_19536
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
